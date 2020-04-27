565b601dc40ecfe1818cbd0de2c0302e93b755ca
Paul Cardenas <pcardenas0011@gmail.com>
2020-04-27 15:42:01 -0400

    Added files

---
+++ restdatabaseapplication/src/main/java/com/example/restdatabaseapplication/VehicleDAO.java
@@ -0,0 +1,36 @@
+package com.example.hellorestdatabase;
+
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public class VehicleDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void create(Vehicle vehicle){
+        entityManager.persist(vehicle);
+        return;
+    }
+
+    public Vehicle getById(int id){
+        return entityManager.find(Vehicle.class, id);
+    }
+
+    public void update(Vehicle vehicle){
+        entityManager.merge(vehicle);
+        return;
+    }
+
+    public void delete(Vehicle vehicle){
+        entityManager.remove(vehicle);
+        return;
+    }
+
+
+}
