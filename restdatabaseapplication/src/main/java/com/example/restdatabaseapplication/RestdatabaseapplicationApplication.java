34d2b7dc9e51634e76351c04df7acaf69a1c8147
Paul Cardenas <pcardenas0011@gmail.com>
2020-04-27 17:41:25 -0400

    Up

--- restdatabaseapplication/src/main/java/com/example/restdatabaseapplication/RestdatabaseapplicationApplication.java
+++ restdatabaseapplication/src/main/java/com/example/restdatabaseapplication/RestdatabaseapplicationApplication.java
@@ -2,11 +2,14 @@ package com.example.restdatabaseapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RestdatabaseapplicationApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestdatabaseapplicationApplication.class, args);
 	}
 
